02eceb36baee1d00ebff84c6fd0daefdce549745
theme <therne@gmail.com>
2024-12-01 14:12:31 -0500

    day1-scala final

--- aoc-2024-scala/src/main/scala/Day1.scala
+++ aoc-2024-scala/src/main/scala/Day1.scala
@@ -1,34 +1,46 @@
+import scala.io.Source
+
 object Day1:
   def calculateTotalDistance(left: Seq[Int], right: Seq[Int]): Int =
     val sortedLeft = left.sorted
     val sortedRight = right.sorted
-    
+
     sortedLeft.zip(sortedRight)
       .map((l, r) => Math.abs(l - r))
       .sum
+
+  def calculateSimilarityScore(left: Seq[Int], right: Seq[Int]): Int =
+    // Count occurrences in right list
+    val rightCounts = right.groupBy(identity).view.mapValues(_.size).toMap
+
+    // For each number in left list, multiply by its count in right list
+    left.map(num => num * rightCounts.getOrElse(num, 0)).sum
 
   def parseInput(input: String): (Seq[Int], Seq[Int]) =
     val pairs = input.trim.split("\n").map { line =>
       val numbers = line.trim.split("\\s+").map(_.toInt)
       (numbers(0), numbers(1))
     }
-    
+
     val (left, right) = pairs.unzip
     (left.toSeq, right.toSeq)
 
-  def solve(input: String): Int =
+  def solvePart1(input: String): Int =
     val (left, right) = parseInput(input)
     calculateTotalDistance(left, right)
 
-@main def day1(): Unit =
-  val exampleInput = """
-    |3 4
-    |4 3
-    |2 5
-    |1 3
-    |3 9
-    |3 3
-    """.stripMargin
+  def solvePart2(input: String): Int =
+    val (left, right) = parseInput(input)
+    calculateSimilarityScore(left, right)
 
-  val result = Day1.solve(exampleInput)
-  println(s"Result: $result")
+  def readFile(filename: String): String =
+    val source = Source.fromFile(filename)
+    try source.mkString finally source.close()
+
+@main def day1(filename: String): Unit =
+  val input = Day1.readFile(filename)
+  val part1Result = Day1.solvePart1(input)
+  val part2Result = Day1.solvePart2(input)
+
+  println(s"Part 1 Result: $part1Result")
+  println(s"Part 2 Result: $part2Result")
